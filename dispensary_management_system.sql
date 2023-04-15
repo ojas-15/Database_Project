23bbaf3cc2012f2f6692e2b35348dc08f5d39ad4
Akshat_Shrivastava <akshat.shrivastava@iitgn.ac.in>
2023-04-15 19:49:31 +0530

    register/sighup messages

--- dispensary_management_system.sql
+++ dispensary_management_system.sql
@@ -416,22 +416,6 @@ INSERT INTO users (name, email, password, role) VALUES
     ('Yash Kokane', 'yash.kokane@gmail.com', '123456', 'admin'),
     ('Akshat Shrivastava', 'akshat.shrivastava@gmail.com', '123456', 'admin');
 
--- DROP TABLE IF EXISTS users;
--- CREATE TABLE users (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     name VARCHAR(255) NOT NULL,
---     email VARCHAR(255) NOT NULL UNIQUE,
---     password VARCHAR(255) NOT NULL,
---     role VARCHAR(255) NOT NULL
--- );
-
--- INSERT INTO users (name, email, password, role) VALUES
---     ('Rakesh Kumar', 'rakesh.kumar@gmail.com', '123456', 'patient'),
---     ('Sunita Gupta', 'sunita.gupta@gmail.com', '123456', 'patient'),
---     ('Anupama Singh', 'anupama.singh@gmail.com', '123456', 'doctor'),
---     ('Rohan Mehra', 'rohan.mehta@gmail.com', '123456', 'doctor'),
---     ('Yash Kokane', 'yash.kokane@gmail.com', '123456', 'admin'),
--- 	('Akshat', 'akshat.shrivastava@gmail.com', '123456', 'admin');
 
 CREATE VIEW Prescription_View AS 
 SELECT 
